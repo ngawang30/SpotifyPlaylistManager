9fc9a915ae990baf82e6837dd2bd49bd9e72d3bc
ngawang30 <ngawang30@gmail.com>
2024-02-12 18:12:49 -0500

    Added Feature: Playlist Reading

--- src/main/java/music/spotifyplaylistmanager/SPM.java
+++ src/main/java/music/spotifyplaylistmanager/SPM.java
@@ -7,10 +7,13 @@ import java.io.OutputStream;
 import java.io.InputStream;
 import java.util.Scanner;
 import org.json.JSONObject;
+import org.json.JSONArray;
+import java.io.File;
+import java.io.FileWriter;
 
 public class SPM {
 	public static void main(String[] args){
-		System.out.println(getPlaylist("6zbr9annOmLVvxWOfM3SBM"));
+		getPlaylistTracks("6zbr9annOmLVvxWOfM3SBM");
 		
 		
 		
@@ -24,19 +27,27 @@ public class SPM {
 		
 	}
 
+	public static void writeToFile(String json){
+		try(FileWriter fw = new FileWriter(new File("tracks.txt"))){
+			fw.write(json);
+			fw.close();
+		} catch (Exception e){}
+
+	}
+
 	public static String getRequestResponse(String url){
 		String response = "asd";
 
 		try{
-		URL urlObj = new URL(url);
-		HttpsURLConnection con = (HttpsURLConnection) urlObj.openConnection();
+			URL urlObj = new URL(url);
+			HttpsURLConnection con = (HttpsURLConnection) urlObj.openConnection();
 		
-		con.setRequestMethod("GET");
-		con.setRequestProperty("Authorization","Bearer " + getToken());
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Authorization","Bearer " + getToken());
 
-		InputStream is = con.getInputStream();
+			InputStream is = con.getInputStream();
 
-		response = new String(is.readAllBytes());
+			response = new String(is.readAllBytes());
 
 		
 		} catch (Exception e){}
@@ -46,9 +57,29 @@ public class SPM {
 
 	}
 
-	public static String getPlaylist(String playlistID){
-		return(getRequestResponse("https://api.spotify.com/v1/playlists/" + playlistID));
+	public static JSONArray getPlaylistTracks(String playlistID){
+		int offSet = 50;
+		int limit = 50;
+		JSONArray allTracks = new JSONArray();
+		String response = getRequestResponse("https://api.spotify.com/v1/playlists/" + playlistID + "/tracks?limit=50");
+		JSONObject responseJSON = new JSONObject (response);
+		allTracks.putAll(responseJSON.getJSONArray("items"));
+		
+		int size = responseJSON.getInt("total");
 
+		while(offSet < size){
+			System.out.println("a");
+			response = getRequestResponse("https://api.spotify.com/v1/playlists/" + playlistID + "/tracks?limit=50&offset=" + offSet);
+			responseJSON = new JSONObject (response);
+			allTracks.putAll(responseJSON.getJSONArray("items"));
+
+			offSet += 50;
+		}
+
+		writeToFile(allTracks.toString());
+
+
+		return (allTracks);
 	}
 
 	public static String getTrackInfo(String trackID){
@@ -64,29 +95,29 @@ public class SPM {
 		JSONObject tokenJSON = new JSONObject();
 
 		try {
-		URL urlObj = new URL("https://accounts.spotify.com/api/token");
-		HttpsURLConnection con = (HttpsURLConnection) urlObj.openConnection();
+			URL urlObj = new URL("https://accounts.spotify.com/api/token");
+			HttpsURLConnection con = (HttpsURLConnection) urlObj.openConnection();
 
-		con.setDoOutput(true);
-		con.setDoInput(true);
+			con.setDoOutput(true);
+			con.setDoInput(true);
 
-		con.setRequestMethod("POST");
-		con.setRequestProperty("Authorization","Basic " + login);
-		con.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Authorization","Basic " + login);
+			con.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
 
-		String requestBody = "grant_type=client_credentials";
+			String requestBody = "grant_type=client_credentials";
 
-		try(OutputStream os = con.getOutputStream()){
-			byte[] input = requestBody.getBytes("UTF-8");
-			os.write(input);
-		} catch (Exception e){}
+			try(OutputStream os = con.getOutputStream()){
+				byte[] input = requestBody.getBytes("UTF-8");
+				os.write(input);
+			} catch (Exception e){}
 		
-		InputStream is = con.getInputStream();
-		response = new String(is.readAllBytes());
+			InputStream is = con.getInputStream();
+			response = new String(is.readAllBytes());
+	
+			tokenJSON = new JSONObject(response);
 
-		tokenJSON = new JSONObject(response);
-
-		con.disconnect();
+			con.disconnect();
 		} catch (Exception e){}
 
 	
